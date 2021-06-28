bcaf3433e2748400444459e3aa8485fd8e57d215
Khalil Lindsay <lindsay.khalil@gmail.com>
2021-06-28 10:36:59 -0400

    commit tree d8baffdc4f4bd0de7b4197d1bd338c9b0d15de9d

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
-// Write your code here
+const num1 = 31
+const num2 = 2
+let multiply = num1*num2;
+const random = Math.floor( Math.random() *2000) + 1;
+const num3 = 16
+const num4 = 4
+const mod = num3/num4;
+const max = Math.max(1,2,3,4,5,20)
+// Write your code here\
